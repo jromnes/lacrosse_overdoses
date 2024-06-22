c64870c2a9b79286b9289f7805774c5843beefde
jromnes <jordan.lucille710@gmail.com>
2024-06-22 13:44:14 -0500

    Jupyter notebooks with data cleasning added to repo. Images directory added, as well as original and cleansed data sets as csv files. Also includes .html report file.

---
+++ .ipynb_checkpoints/functions-checkpoint.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from collections import Counter
+
+def find_columns_with_ones(df):
+    # Initialize an empty dictionary to store the results
+    result = {}
+    
+    # Iterate over each row in the DataFrame
+    for index, row in df.iterrows():
+        # Find the columns with a value of 1
+        columns_with_one = tuple(sorted([col for col in df.columns if row[col] == 1.0]))
+        
+        # Store the result in the dictionary with ID as key
+        result[index] = columns_with_one
+    
+    return result
